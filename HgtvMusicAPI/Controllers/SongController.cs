31653a3c8a2216d388274e6b217ea7f6f4e15d78
quachhuy <2051060555@e.tlu.edu.vn>
2024-03-14 22:15:34 +0700

    them song controller.them authentication

--- HgtvMusicAPI/Controllers/SongController.cs
+++ HgtvMusicAPI/Controllers/SongController.cs
@@ -6,170 +6,110 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using HgtvMusicAPI.Data;
+using HgtvMusicAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HgtvMusicAPI.Controllers
 {
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
     public class SongController : Controller
     {
         private readonly MyDbContext _context;
-
         public SongController(MyDbContext context)
         {
             _context = context;
-        }
 
-        // GET: Song
-        public async Task<IActionResult> Index()
-        {
-            var myDbContext = _context.Songs.Include(s => s.Album).Include(s => s.Category).Include(s => s.Singer);
-            return View(await myDbContext.ToListAsync());
-        }
 
-        // GET: Song/Details/5
-        public async Task<IActionResult> Details(int? id)
+        }
+        // lay danh sach Song
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            if (id == null)
+            var songs = _context.Singers.ToList();
+            return Ok(songs);
+
+        }
+        // lay Song qua ID
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            var songs = _context.Songs.SingleOrDefault(si => si.IdSong == id);
+            if (songs == null)
             {
                 return NotFound();
             }
-
-            var song = await _context.Songs
-                .Include(s => s.Album)
-                .Include(s => s.Category)
-                .Include(s => s.Singer)
-                .FirstOrDefaultAsync(m => m.IdSong == id);
-            if (song == null)
+            else
             {
-                return NotFound();
+                return Ok(songs);
             }
-
-            return View(song);
         }
 
-        // GET: Song/Create
-        public IActionResult Create()
-        {
-            ViewData["IdAlbum"] = new SelectList(_context.Albums, "IdAlbum", "NameAlbum");
-            ViewData["IdCategory"] = new SelectList(_context.Set<Category>(), "IdCategory", "NameCategory");
-            ViewData["IdSinger"] = new SelectList(_context.Singers, "IdSinger", "NameSinger");
-            return View();
-        }
-
-        // POST: Song/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // tao moi song
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdSong,NameSong,Path_Img,IdCategory,IdSinger,IdAlbum")] Song song)
+        public IActionResult CreateSong(SongModel model)
         {
-            if (ModelState.IsValid)
+            try
             {
+                var song = new Song
+                {
+                    NameSong = model.NameSong,
+                    Path_Img = model.Path_Img,
+                };
                 _context.Add(song);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                _context.SaveChanges();
+                return Ok(song);
             }
-            ViewData["IdAlbum"] = new SelectList(_context.Albums, "IdAlbum", "NameAlbum", song.IdAlbum);
-            ViewData["IdCategory"] = new SelectList(_context.Set<Category>(), "IdCategory", "NameCategory", song.IdCategory);
-            ViewData["IdSinger"] = new SelectList(_context.Singers, "IdSinger", "NameSinger", song.IdSinger);
-            return View(song);
+            catch
+            {
+                return BadRequest();
+            }
+
         }
 
-        // GET: Song/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var song = await _context.Songs.FindAsync(id);
-            if (song == null)
+        // chinh sua song.
+        [HttpPut("id")]
+        public IActionResult UpdateSingerById(int id, SongModel model)
+        {
+            var so = _context.Songs.SingleOrDefault(si => si.IdSong == id);
+            if (so == null)
             {
                 return NotFound();
             }
-            ViewData["IdAlbum"] = new SelectList(_context.Albums, "IdAlbum", "NameAlbum", song.IdAlbum);
-            ViewData["IdCategory"] = new SelectList(_context.Set<Category>(), "IdCategory", "NameCategory", song.IdCategory);
-            ViewData["IdSinger"] = new SelectList(_context.Singers, "IdSinger", "NameSinger", song.IdSinger);
-            return View(song);
+            else
+            {
+                so.NameSong = model.NameSong;
+                so.Path_Img = model.Path_Img;
+                _context.SaveChanges();
+                return NoContent();
+            }
         }
 
-        // POST: Song/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdSong,NameSong,Path_Img,IdCategory,IdSinger,IdAlbum")] Song song)
+        //Xoa ban ghi voi ID
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
         {
-            if (id != song.IdSong)
+            try
             {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                var song = _context.Songs.FirstOrDefault(e => e.IdSinger == id);
+                if (song != null)
                 {
-                    _context.Update(song);
-                    await _context.SaveChangesAsync();
+                    _context.Songs.Remove(song);
+                    _context.SaveChanges();
+                    return Ok($"Deleted item with ID: {id}");
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SongExists(song.IdSong))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                return Ok($"Deleted item with ID: {id}");
             }
-            ViewData["IdAlbum"] = new SelectList(_context.Albums, "IdAlbum", "NameAlbum", song.IdAlbum);
-            ViewData["IdCategory"] = new SelectList(_context.Set<Category>(), "IdCategory", "NameCategory", song.IdCategory);
-            ViewData["IdSinger"] = new SelectList(_context.Singers, "IdSinger", "NameSinger", song.IdSinger);
-            return View(song);
-        }
-
-        // GET: Song/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                // Xử lý lỗi nếu có
+                Console.WriteLine($"Error deleting item with ID: {id}. Error message: {ex.Message}");
+                return StatusCode(500, $"Error deleting item with ID: {id}");
             }
-
-            var song = await _context.Songs
-                .Include(s => s.Album)
-                .Include(s => s.Category)
-                .Include(s => s.Singer)
-                .FirstOrDefaultAsync(m => m.IdSong == id);
-            if (song == null)
-            {
-                return NotFound();
-            }
-
-            return View(song);
         }
 
-        // POST: Song/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var song = await _context.Songs.FindAsync(id);
-            if (song != null)
-            {
-                _context.Songs.Remove(song);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool SongExists(int id)
-        {
-            return _context.Songs.Any(e => e.IdSong == id);
-        }
     }
 }
--- HgtvMusicAPI/Models/SongModel.cs
+++ HgtvMusicAPI/Models/SongModel.cs
@@ -4,8 +4,6 @@ namespace HgtvMusicAPI.Models
 {
     public class SongModel
     {
-        [MaxLength(50)]
-        [Required]
         public string NameSong { get; set; }
 
         public string? Path_Img { get; set; }
